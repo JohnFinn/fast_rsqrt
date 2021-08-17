8fb67957a0d6f10153066f2b20edffe5f7bd0ec1
Jouni Sunnari <dz4tune@gmail.com>
2021-08-17 23:09:54 +0300

    inverse square root implementations

---
+++ rsqrt.cpp
@@ -0,0 +1,22 @@
+#include "rsqrt.hpp"
+
+#include <cmath>
+
+float rsqrt(float n) {
+    return 1 / sqrt(n);
+}
+
+float Q_rsqrt(float number) {
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    y = number;
+    i = *(long*) &y;
+    i = 0x5f3759df - ( i >> 1 );
+    y = *(float*) &i;
+    y = y * ( threehalfs - ( x2 * y * y ));
+    //y = y * ( threehalfs - ( x2 * y * y ));
+    return y;
+}
---
+++ rsqrt.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+float rsqrt(float);
+
+float Q_rsqrt(float);
