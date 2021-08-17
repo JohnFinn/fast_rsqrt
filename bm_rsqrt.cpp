ec4c6a144a6545dcf1ee554d562b436f09c009ed
Jouni Sunnari <dz4tune@gmail.com>
2021-08-17 23:40:15 +0300

    benchmarks

---
+++ bm_rsqrt.cpp
@@ -0,0 +1,34 @@
+#include <benchmark/benchmark.h>
+#include "rsqrt.hpp"
+#include <random>
+
+static void BM_rsqrt(benchmark::State& state)
+{
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_real_distribution<float> dis{0.1, 100000};
+    for (auto _: state) {
+        //state.PauseTiming();
+        //float num = dis(gen);
+        //state.ResumeTiming();
+        rsqrt(123.6789);
+    }
+}
+
+static void BM_Q_rsqrt(benchmark::State& state)
+{
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_real_distribution<float> dis{0.1, 100000};
+    for (auto _: state) {
+        //state.PauseTiming();
+        //float num = dis(gen);
+        //state.ResumeTiming();
+        Q_rsqrt(123.6789);
+    }
+}
+
+BENCHMARK(BM_rsqrt);
+BENCHMARK(BM_Q_rsqrt);
+
+BENCHMARK_MAIN();
